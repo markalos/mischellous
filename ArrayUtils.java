7144d94683aab0f171b9f37a1e991cbe1210e36a
nighowl <2513195842@qq.com>
2017-12-09 18:42:49 +0800

    java utils

---
+++ ArrayUtils.java
@@ -0,0 +1,8 @@
+
+
+
+class ArrayUtils  {
+	public static boolean isArray(Object obj) {
+		return obj.getClass().isArray();
+	}
+}
---
+++ StringUtils.java
@@ -0,0 +1,11 @@
+
+
+class StringUtils {
+	String objectToString(Object obj) {
+		if (ArrayUtils.isArray(obj)) {
+			for (e : obj) {
+				
+			}
+		}
+	}
+}
